51933c51069347619e97dcc69bd555104d5a5818
“kasper341” <kasper_341@hotmail.com>
2017-05-31 23:13:43 -0700

    update debugging statements

---
+++ app/reducers/root.js
@@ -0,0 +1,70 @@
+import { combineReducers } from 'redux'
+
+const recipesReducer = (recipes = [], action) => {
+  switch (action.type) {
+    case 'ADD_RECIPE':
+    console.log('ADD_RECIPE')
+      // console.log(action)
+      // console.log('state ', recipes.concat({name: action.name}))
+      // console.log(action.name)
+
+      return recipes.concat({name: action.name})
+
+  }
+
+  return recipes
+}
+
+const ingriedientsReducer = (ingriedients = [], action) => {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      console.log('ADD_INGEDIENT')
+      // console.log(state)
+
+      const newIngredient = {
+        recipe: action.recipe,
+        name: action.name,
+        quantity: action.quantity
+      }
+
+      return ingriedients.concat(newIngredient)
+
+  }
+
+  return ingriedients
+}
+
+const initialState = {
+  recipes: [
+    {
+      name: 'Omelette'
+    }
+  ],
+  ingriedients: [
+    {
+      recipe: 'Omlette',
+      name: 'Egg',
+      quantity: 2
+    }
+  ]
+}
+
+export default combineReducers({
+  recipes: recipesReducer,
+  ingriedients: ingriedientsReducer
+})
+// const rootReducer = (state = initialState, action) => {
+//   return Object.assign({}, state, {
+//      recipes: recipesReducer(state.recipes, action),
+//      ingriedients: ingriedientsReducer(state.ingriedients, action)
+//    })
+// }
+
+// const store = createStore(reducer, initialState)
+
+// store.subscribe(() => document.getElementById('counter').innerText = store.getState());
+
+// setInterval(() => store.dispatch({ type: 'INC' }), 500);
+
+// export default rootReducer
+
